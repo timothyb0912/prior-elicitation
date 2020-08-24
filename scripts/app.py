ef2152fa86aa68a6f7accc6499345f9e015c7e55
Timothy Brathwaite <timothyb0912@gmail.com>
2020-08-23 19:16:44 -0700

    [WIP] Pseudocode for the app v1

---
+++ scripts/app.py
@@ -0,0 +1,12 @@
+"""
+UI for prior understanding via interactive, prior setting and predictive plots.
+"""
+import papermill
+import streamlit as st
+import pyprojroot
+
+# Create each of the needed arguments for the notebook
+
+# Run the notebook
+
+# Load and display the created image
